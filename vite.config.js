57f9ba9d7da26a6f75e18a306ac278775bcc7848
Manuel Aguilera <manuel.aguilera.vr@gmail.com>
2026-03-18 13:23:16 -0600

    con docs

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,15 @@
 import { defineConfig } from "vite";
 
+import { resolve } from "path";
+
 export default defineConfig({
   base: "./", //Usa rutas relativas en los enlaces generados en el HTML
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        location: resolve(__dirname, "location.html"),
+      },
+    },
+  },
 });
